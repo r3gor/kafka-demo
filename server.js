d7314d1ae05c351ffbeaedd4ae5a1f680b25a0c0
DAN <daercimi@gmail.com>
2021-06-30 17:20:35 -0500

    feat: Se logró mandar datos al server.js

--- server.js
+++ server.js
@@ -8,14 +8,18 @@ const port = 3000;
 app.use(Express.static("public"));
 app.use(Express.json());
 app.use(morgan('tiny'))
+app.use(Express.urlencoded({extended: false}))
 
 app.get("/", async (req, res) => {
     res.sendFile(path.resolve('index.html'))
 })
 
 app.post("/", async (req, res) => {
-    //console.log("contenido del POST: ", req);
+    const { nombre } = req.body
+
+    console.log("Nombre del Estudiante: ", nombre);
     res.sendFile(path.resolve('public/html/listening.html'))
 })
 
 app.listen(port, () => console.log("Listening on port " + port));
+
