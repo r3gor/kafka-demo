26b5d40bbc04b30003895f7c80329c79c5b441c4
DAN <daercimi@gmail.com>
2021-06-30 03:43:37 -0500

    feat: Añadido formulario de entrada

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 import Express from "express";
+import * as path from 'path';
+import morgan from "morgan";
 
 const app = Express();
 const port = 3000;
 
 app.use(Express.static("public"));
 app.use(Express.json());
+app.use(morgan('tiny'))
 
 app.get("/", async (req, res) => {
     res.sendFile(path.resolve('index.html'))
+})
+
+app.post("/", async (req, res) => {
+    //console.log("contenido del POST: ", req);
+    res.sendFile(path.resolve('public/html/listening.html'))
 })
 
 app.listen(port, () => console.log("Listening on port " + port));
